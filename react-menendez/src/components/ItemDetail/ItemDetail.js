fe74f04c055f36f104dc27d10b5e3be12a2ef19a
Juan Pablo Menéndez Rodríguez <pablomero@gmail.com>
2022-04-04 11:40:38 -0300

    update para primera entraga del proyecto final

--- react-menendez/src/components/ItemDetail/ItemDetail.js
+++ react-menendez/src/components/ItemDetail/ItemDetail.js
@@ -1,11 +1,6 @@
 import './ItemDetail.css';
-import ItemCount from '../ItemCount/ItemCount'
 
 const ItemDetail = ({ id, name, img, category, description, price, stock }) => {
-  // function on add para agregar al carrito
-  // const onAdd = () => {
-  //   console.log('Funcion para agregar al carrito')
-  // }
 
   return (
       <div className="itemDetail">
@@ -13,8 +8,6 @@ const ItemDetail = ({ id, name, img, category, description, price, stock }) => {
         <h1>{name} - {category}</h1>
         <p>{description}</p>
         <p>Stock: {stock} - Precio: ${price}</p>
-        // Sincronizar counter con el carrito
-        // <ItemCount initial={0} stock={stock} onAdd={onAdd} />
       </div>
   )
 }
